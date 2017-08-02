b8cc6a004961d5ddb6e589132402bf38028913d7
ouxiong <ouxiong1995@gmail.com>
2017-08-02 22:33:34 +0800

    change classfiyComponennt

--- src/components/ClassifyComponents/ClassifyList.vue
+++ src/components/ClassifyComponents/ClassifyList.vue
@@ -29,12 +29,13 @@ export default {
             var that = this;
           jsonp('https://simba-api.meilishuo.com/venus/mce/v1/urlMakeUpChange/h5?channel=wap&page=1&pageSize=30&pid='+ this.pid,{callback:'jsonp1',},function(err, data){
               if(data){
-                  // console.log(data)
+                  console.log(data)
                   // console.log(data.value.category_1.list)
                   // console.log(data.value.category_2.list)
-                  //  console.log(that.itemData)
-                  that.itemData.push(data.value.category_1);
-                  that.itemData.push(data.value.category_2);
+                  that.itemData.push((data.value.category_1)?(data.value.category_1):'');
+                  that.itemData.push((data.value.category_2)?(data.value.category_2):(data.value.category_1));
+                   console.log(that.itemData)
+                  
                  
                   
 
